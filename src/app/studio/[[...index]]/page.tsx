5c2df0e9ebf00a85b47259d27a498e6f31593954
Henry Sterling <henry@servefirst.co.uk>
2026-04-03 11:10:54 +0000

    Fix Sanity Studio SSR and styled-components config

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../../sanity.config";
+import dynamic from "next/dynamic";
 
-export default function StudioPage() {
-  return <NextStudio config={config} />;
+const StudioPage = dynamic(
+  () => import("next-sanity/studio").then((mod) => {
+    const { NextStudio } = mod;
+    const config = require("../../../../sanity.config").default;
+    return { default: () => <NextStudio config={config} /> };
+  }),
+  { ssr: false, loading: () => <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>Loading Studio...</div> }
+);
+
+export default function Studio() {
+  return <StudioPage />;
 }
